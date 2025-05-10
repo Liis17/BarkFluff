4d121be2b77d3ec956fe5e3b10bb0fab0dab1af4
Славик Лис <Fooxboy@yandex.ru>
2025-05-11 01:53:32 +0300

    324423234

--- Backend/BarkFluff.Files/Program.cs
+++ Backend/BarkFluff.Files/Program.cs
@@ -56,6 +56,5 @@ public class Program
         app.MapGrpcService<FilesServerApiService>();
 
         app.Run();
-        
     }
 }
--- Backend/BarkFluff.Identity/Program.cs
+++ Backend/BarkFluff.Identity/Program.cs
@@ -74,5 +74,6 @@ public class Program
         app.MapGrpcService<IdentityApiService>();
 
         app.Run();
+        
     }
 }
