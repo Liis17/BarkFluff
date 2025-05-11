a31ceae756c2e1490bf5429cc37bd5d84f5cd469
Славик Лис <Fooxboy@yandex.ru>
2025-05-11 18:54:58 +0300

    Добавлены исправления исправлений

---
+++ BarkFluff.Shared.Exceptions/Accounts/UserIsDraftException.cs
@@ -0,0 +1,8 @@
+namespace BarkFluff.Shared.Exceptions.Accounts;
+
+public class UserIsDraftException : BaseGrpcException
+{
+    public override string ErrorCode => "91D75288-8314-4658-AA0E-EC1D01779D58";
+
+    public override string ErrorMessage => "Пользователь с такими параметрами есть, но он не подтвержден";
+}
