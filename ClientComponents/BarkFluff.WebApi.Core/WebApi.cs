8822622c25d2ef287d1c1d64aa457e8fbf56f7c3
Li_is <daskent96@gmail.com>
2025-05-14 01:41:13 +0300

    Добавлены проекты и поддержка gRPC в BarkFluff
    В проекте BarkFluff.sln добавлены новые проекты: "ClientComponents" и "BarkFluff.WebApi.Core".
    В проекте BarkFluff.WebApi.Core.csproj настроено использование .NET 9.0 и добавлены ссылки на пакеты Google.Protobuf и Grpc.Net.Client.
    Изменено пространство имен в файле GlobalParam.cs на BarkFluff.WebApi.Core.MessengerData, обновлены директивы using.
    В файле WebApi.cs реализованы новые классы и методы для работы с gRPC, включая создание клиентов для различных API и добавление интерсепторов для аутентификации.

---
+++ ClientComponents/BarkFluff.WebApi.Core/WebApi.cs
@@ -0,0 +1,48 @@
+﻿
+using BarkFluff.WebApi.Core.MessengerData;
+
+using Grpc.Core.Interceptors;
+using Grpc.Net.Client;
+
+namespace BarkFluff.WebApi.Core
+{
+    public class WebApi
+    {
+        public BarkFluff.Proto.Users.UsersApi.UsersApiClient UsersAC;
+        public BarkFluff.Proto.Beacon.BeaconApi.BeaconApiClient BeaconAC;
+        public BarkFluff.Proto.Identity.IdentityApi.IdentityApiClient IdentityAC;
+
+        public GrpcChannel BeaconChannel;
+        public GrpcChannel UserChannel;
+        public GrpcChannel IdentityChannel;
+
+        public void CreateBeaconAC(GlobalParam _gParam)
+        {
+            _gParam.SocketBeacon = EnsureHttpPrefix(_gParam.SocketBeacon);
+            BeaconChannel = GrpcChannel.ForAddress(_gParam.SocketBeacon);
+            BeaconAC = new BarkFluff.Proto.Beacon.BeaconApi.BeaconApiClient(BeaconChannel);
+        }
+
+        public void AddInterceptor(GlobalParam _gParam, string _deviceName)
+        {
+            var deviceInterceptor = new Shared.Auth.XDeviceClientInterceptor(deviceName: _deviceName);
+            var jwtInterceptor = new Shared.Auth.JwtClientInterceptor(string.Empty);
+            IdentityChannel = GrpcChannel.ForAddress(_gParam.SocketIdentity);
+            UserChannel = GrpcChannel.ForAddress(_gParam.SocketUsers);
+
+            var identityInvoker = IdentityChannel.Intercept(deviceInterceptor).Intercept(jwtInterceptor);
+            var userInvoker = UserChannel.Intercept(deviceInterceptor).Intercept(jwtInterceptor);
+
+            IdentityAC = new BarkFluff.Proto.Identity.IdentityApi.IdentityApiClient(identityInvoker);
+            UsersAC = new BarkFluff.Proto.Users.UsersApi.UsersApiClient(userInvoker);
+
+        }
+
+        private string EnsureHttpPrefix(string _url)
+        {
+            return !_url.StartsWith("http://") && !_url.StartsWith("https://")
+                   ? "http://" + _url
+                   : _url;
+        }
+    }
+}
