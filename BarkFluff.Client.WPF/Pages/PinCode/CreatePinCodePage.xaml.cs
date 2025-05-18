8adbb577448a8c93cfc15f26287528769bd757a4
Li_is <daskent96@gmail.com>
2025-05-18 18:20:31 +0300

    Добавлен интерфейс и логика для ввода пин-кода
    В файле `CreatePinCodePage.xaml` реализован интерфейс для создания пин-кода с текстовыми блоками, полями ввода и кнопкой продолжения. Страница организована с использованием `Grid` и `StackPanel`, а также добавлены стили и обработчики событий.
    
    В файле `CreatePinCodePage.xaml.cs` реализована логика взаимодействия с элементами управления, включая обработку ввода текста, переключение фокуса между полями ввода и проверку на совпадение введенных пин-кодов.
    
    В файле `PinCodePage.xaml` добавлен интерфейс для ввода пин-кода с текстовыми блоками, полями ввода и индикаторами попыток, использующий `StackPanel`.
    
    В файле `PinCodePage.xaml.cs` реализована логика для обработки ввода пин-кода, включая проверку на корректность ввода, управление попытками и переход к следующему шагу при успешном вводе.

---
+++ BarkFluff.Client.WPF/Pages/PinCode/CreatePinCodePage.xaml.cs
@@ -0,0 +1,210 @@
+﻿using BarkFluff.WebApi.Core.MessengerData;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace BarkFluff.Client.WPF.Pages.PinCode
+{
+    /// <summary>
+    /// Логика взаимодействия для CreatePinCodePage.xaml
+    /// </summary>
+    public partial class CreatePinCodePage : UserControl
+    {
+        private string firstPinCode = "";
+        private string secondPinCode = "";
+
+        private TextBox[] firstBoxes;
+        private TextBox[] secondBoxes;
+        public CreatePinCodePage()
+        {
+            InitializeComponent();
+            firstBoxes = new TextBox[] { FirstBox0, FirstBox1, FirstBox2, FirstBox3 };
+            secondBoxes = new TextBox[] { SecondBox0, SecondBox1, SecondBox2, SecondBox3 };
+
+            foreach (var box in firstBoxes)
+            {
+                box.MaxLength = 1;
+            }
+
+            foreach (var box in secondBoxes)
+            {
+                box.MaxLength = 1;
+            }
+            ErrorTextBlockMessge.Text = string.Empty;
+        }
+
+        private void FirstPinBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox currentBox = sender as TextBox;
+            int currentIndex = Array.IndexOf(firstBoxes, currentBox);
+
+            if (!string.IsNullOrEmpty(currentBox.Text))
+            {
+                if (currentIndex < firstBoxes.Length - 1)
+                {
+                    firstBoxes[currentIndex + 1].Focus();
+                }
+                else
+                {
+                    secondBoxes[0].Focus();
+                }
+            }
+
+            UpdateFirstPinCode();
+        }
+
+        private void SecondPinBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox currentBox = sender as TextBox;
+            int currentIndex = Array.IndexOf(secondBoxes, currentBox);
+
+            if (!string.IsNullOrEmpty(currentBox.Text) && currentIndex < secondBoxes.Length - 1)
+            {
+                secondBoxes[currentIndex + 1].Focus();
+            }
+
+            UpdateSecondPinCode();
+        }
+
+        private void PinBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void FirstPinBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox currentBox = sender as TextBox;
+            int currentIndex = Array.IndexOf(firstBoxes, currentBox);
+
+            if (e.Key == Key.Back)
+            {
+                if (string.IsNullOrEmpty(currentBox.Text) && currentIndex > 0)
+                {
+                    firstBoxes[currentIndex - 1].Focus();
+                    firstBoxes[currentIndex - 1].CaretIndex = firstBoxes[currentIndex - 1].Text.Length;
+                }
+            }
+        }
+
+        private void SecondPinBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox currentBox = sender as TextBox;
+            int currentIndex = Array.IndexOf(secondBoxes, currentBox);
+
+            if (e.Key == Key.Back)
+            {
+                if (string.IsNullOrEmpty(currentBox.Text))
+                {
+                    if (currentIndex == 0 && !string.IsNullOrEmpty(firstBoxes[firstBoxes.Length - 1].Text))
+                    {
+                        firstBoxes[firstBoxes.Length - 1].Focus();
+                        firstBoxes[firstBoxes.Length - 1].CaretIndex = firstBoxes[firstBoxes.Length - 1].Text.Length;
+                    }
+                    else if (currentIndex > 0)
+                    {
+                        secondBoxes[currentIndex - 1].Focus();
+                        secondBoxes[currentIndex - 1].CaretIndex = secondBoxes[currentIndex - 1].Text.Length;
+                    }
+                }
+            }
+        }
+
+        private void PinBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox focusedBox = sender as TextBox;
+
+            int firstBoxIndex = Array.IndexOf(firstBoxes, focusedBox);
+            if (firstBoxIndex != -1)
+            {
+                for (int i = 0; i < firstBoxIndex; i++)
+                {
+                    if (string.IsNullOrEmpty(firstBoxes[i].Text))
+                    {
+                        firstBoxes[i].Focus();
+                        return;
+                    }
+                }
+            }
+
+            int secondBoxIndex = Array.IndexOf(secondBoxes, focusedBox);
+            if (secondBoxIndex != -1)
+            {
+                for (int i = 0; i < firstBoxes.Length; i++)
+                {
+                    if (string.IsNullOrEmpty(firstBoxes[i].Text))
+                    {
+                        firstBoxes[i].Focus();
+                        return;
+                    }
+                }
+
+                for (int i = 0; i < secondBoxIndex; i++)
+                {
+                    if (string.IsNullOrEmpty(secondBoxes[i].Text))
+                    {
+                        secondBoxes[i].Focus();
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void FocusFirstBoxZero(object sender, RoutedEventArgs e)
+        {
+            firstBoxes[0].Focus();
+        }
+
+        private void ContinueButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (firstPinCode.Length == 4 && secondPinCode.Length == 4)
+            {
+                
+                if (firstPinCode != secondPinCode)
+                {
+                    ErrorTextBlockMessge.Text = "Пинкоды не совпадают";
+                }
+                else
+                {
+                    App.GParam.AppPass = firstPinCode;
+                    string filePath = Path.Combine(App.GParam.AppPath, "GlobalParam.json");
+                    GlobalParam.Save(App.GParam, filePath, App.GParam.AppPass);
+                    App.MessengerWindow.OpenServerListPage();
+                }
+            }
+            else
+            {
+                ErrorTextBlockMessge.Text = "Заполните обе строки пинкода";
+            }
+        }
+
+        private void UpdateFirstPinCode()
+        {
+            firstPinCode = "";
+            foreach (var box in firstBoxes)
+            {
+                firstPinCode += box.Text;
+            }
+        }
+
+        private void UpdateSecondPinCode()
+        {
+            secondPinCode = "";
+            foreach (var box in secondBoxes)
+            {
+                secondPinCode += box.Text;
+            }
+        }
+    }
+}
---
+++ BarkFluff.Client.WPF/Pages/PinCode/PinCodePage.xaml.cs
@@ -0,0 +1,200 @@
+﻿using BarkFluff.WebApi.Core.MessengerData;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace BarkFluff.Client.WPF.Pages.PinCode
+{
+    /// <summary>
+    /// Логика взаимодействия для PinCodePage.xaml
+    /// </summary>
+    public partial class PinCodePage : UserControl
+    {
+        private char[] pinDigits = new char[4];
+        private int attempts = 3;
+        public PinCodePage()
+        {
+            InitializeComponent();
+        }
+
+        private void PincodeSecure_Loaded(object sender, RoutedEventArgs e)
+        {
+            KeysIconUpdate();
+            helper.Text = "";
+        }
+
+        private void PinBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(char.IsDigit);
+        }
+
+        private void PinBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox currentBox = (TextBox)sender;
+            int index = GetBoxIndex(currentBox);
+
+            if (string.IsNullOrEmpty(currentBox.Text))
+            {
+                pinDigits[index] = '\0';
+                return;
+            }
+
+            string input = currentBox.Text;
+            if (!char.IsDigit(input[0]))
+                return;
+
+            pinDigits[index] = input[0];
+
+            currentBox.TextChanged -= PinBox_TextChanged;
+            currentBox.Text = "●";
+            currentBox.CaretIndex = 1;
+            currentBox.TextChanged += PinBox_TextChanged;
+
+            if (index < 3)
+            {
+                GetBox(index + 1).Focus();
+            }
+            else
+            {
+                string pin = new string(pinDigits);
+                if (pin.All(char.IsDigit))
+                {
+                    if (IsValid(pin))
+                    {
+                        Next();
+                    }
+                    else
+                    {
+                        if (attempts > 0)
+                        {
+                            attempts--;
+                            KeysIconUpdate();
+                            ClearAll();
+                            GetBox(0).Focus();
+                        }
+                        if (attempts == 0)
+                        {
+                            helper.Text = "Попытки исчерпаны. Помянем";
+                            RemoveSettings(sender, e);
+                        }
+                        else if (attempts == 1)
+                        {
+                            helper.Text = "Последняя попытка ввести правильный код\nПри неудаче последует сброс...";
+                        }
+                        else if (attempts == 2)
+                        {
+                            helper.Text = "Это PIN код можно отключить в настройках";
+                        }
+                    }
+                }
+            }
+        }
+        private void KeysIconUpdate()
+        {
+            key3.Visibility = attempts >= 3 ? Visibility.Visible : Visibility.Collapsed;
+            key2.Visibility = attempts >= 2 ? Visibility.Visible : Visibility.Collapsed;
+            key1.Visibility = attempts >= 1 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void PinBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox currentBox = (TextBox)sender;
+            int index = GetBoxIndex(currentBox);
+
+            if (e.Key == Key.Back)
+            {
+                e.Handled = true;
+                pinDigits[index] = '\0';
+                currentBox.Clear();
+
+                if (index > 0)
+                {
+                    GetBox(index - 1).Focus();
+                    GetBox(index - 1).Clear();
+                    pinDigits[index - 1] = '\0';
+                }
+            }
+        }
+
+        private void PinBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (pinDigits[i] == '\0')
+                {
+                    GetBox(i).Focus();
+                    return;
+                }
+            }
+            GetBox(3).Focus();
+        }
+
+        private TextBox GetBox(int index) => (TextBox)PinContainer.Children[index];
+        private int GetBoxIndex(TextBox box) => PinContainer.Children.IndexOf(box);
+
+        private void ClearAll()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                GetBox(i).Clear();
+                pinDigits[i] = '\0';
+            }
+        }
+
+        private bool IsValid(string pin)
+        {
+            string exePath = Assembly.GetExecutingAssembly().Location;
+            string exeDirectory = Path.GetDirectoryName(exePath);
+            string filePath = Path.Combine(exeDirectory, "GlobalParam.json");
+            if (File.Exists(filePath))
+            {
+                var a = GlobalParam.VerifyPassword(filePath, pin);
+                return a;
+            }
+            else
+            {
+                MessageBox.Show("Файл GlobalParam.json не найден", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+        }
+        private void Next()
+        {
+            string exeDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "GlobalParam.json");
+            App.GParam = GlobalParam.Load(filePath, new string(pinDigits));
+            App.GParam.AppPass = new string(pinDigits);
+            App.GParam.AppPath = exeDirectory ?? string.Empty;
+
+            //App.PincodeSuccessful();
+        }
+
+        private void RemoveSettings(object sender, RoutedEventArgs e)
+        {
+            string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "GlobalParam.json");
+            if (File.Exists(filePath))
+            {
+                File.Delete("GlobalParam.json");
+                Application.Current.Shutdown();
+            }
+
+
+        }
+
+        private void FocusBoxZero(object sender, RoutedEventArgs e)
+        {
+            Box0.Focus();
+        }
+    }
+}
