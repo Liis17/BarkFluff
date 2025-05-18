2ef1e31c7381f3e1fc2600175f3b81ed74cb2bbc
Li_is <daskent96@gmail.com>
2025-05-18 18:21:02 +0300

    Добавлены новые функции регистрации и входа в систему
    Внесены изменения в интерфейс и логику регистрации в файле `CreateAccount.xaml`, включая новые стили, шаги и анимации. Реализованы методы для проверки валидности логина и пароля в `CreateAccount.xaml.cs`.
    
    Обновлен интерфейс входа в систему в `Login.xaml` с добавлением стилей и функционала восстановления пароля.
    
    Добавлены элементы управления для ввода адреса сервера в `SelectServer.xaml` и соответствующая логика в `SelectServer.xaml.cs` для проверки формата адреса и обработки ошибок подключения.
    
    Улучшена навигация в приложении с добавлением кнопки для перехода на страницу создания PIN-кода в `WelcomPage.xaml` и `WelcomPage.xaml.cs`.

--- BarkFluff.Client.WPF/Pages/SetupPages/SelectServer.xaml.cs
+++ BarkFluff.Client.WPF/Pages/SetupPages/SelectServer.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using BarkFluff.WebApi.Core.MessengerData;
+
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,7 +14,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BarkFluff.Client.WPF.Pages.SetupPages
 {
@@ -21,6 +25,53 @@ namespace BarkFluff.Client.WPF.Pages.SetupPages
         public SelectServer()
         {
             InitializeComponent();
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string serverInput = ServerAddressTextBox.Text.Trim();
+
+                if (string.IsNullOrEmpty(serverInput))
+                {
+                    ErrorText.Text = "Укажите адрес сервера и порт";
+                    return;
+                }
+
+                string pattern = @"^([^:]+):(\d+)$";
+                var match = Regex.Match(serverInput, pattern);
+                if (!match.Success)
+                {
+                    ErrorText.Text = "Неверный формат. Используйте [домен или IP]:[порт]";
+                    return;
+                }
+
+                string host = match.Groups[1].Value;
+                string port = match.Groups[2].Value;
+
+                string socket = host + ":" + port;
+                App.GParam.SocketBeacon = socket;
+                App.ServerCommunication.CreateOnlyBeaconAC(App.GParam);
+
+                try
+                {
+                    App.ServerCommunication.GetServerInfo(App.GParam);
+                    GlobalParam.Save(App.GParam, Path.Combine( App.GParam.AppPath, "GlobalParam.json"), App.GParam.AppPass);
+                }
+                catch (Exception ex)
+                {
+                    ErrorText.Text = $"Ошибка подключения: {ex.Message}";
+                    return;
+                }
+
+                App.MessengerWindow.OpenLoginPage();
+
+
+            }
+            catch (Exception ex)
+            {
+                ErrorText.Text = $"Неизвестная ошибка: {ex.Message}";
+            }
         }
     }
 }
