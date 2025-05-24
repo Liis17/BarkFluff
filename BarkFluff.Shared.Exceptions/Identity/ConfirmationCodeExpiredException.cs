a52422216a8054c919653014e6ff33f27a45f558
Li_is <daskent96@gmail.com>
2025-05-25 02:06:03 +0300

    Исправлено сообщение об ошибке в исключении
    В классе `ConfirmationCodeExpiredException` изменено сообщение об ошибке с "Код потверждения больше недействителен" на "Код подтверждения больше недействителен".

--- BarkFluff.Shared.Exceptions/Identity/ConfirmationCodeExpiredException.cs
+++ BarkFluff.Shared.Exceptions/Identity/ConfirmationCodeExpiredException.cs
@@ -4,5 +4,5 @@ public class ConfirmationCodeExpiredException : BaseGrpcException
 {
     public override string ErrorCode => "7AABF347-1210-4B14-A93B-2BA8574D74E7";
 
-    public override string ErrorMessage => "Код потверждения больше недействителен";
+    public override string ErrorMessage => "Код подтверждения больше недействителен";
 }
