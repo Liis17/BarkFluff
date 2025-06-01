0671367657560d65844f9ef9705e6f533a5475a5
Славик Лис <Fooxboy@yandex.ru>
2025-06-01 16:54:40 +0300

    Добавлено описание API для быстрого входа

---
+++ Backend/BarkFluff.FastAuth/Features/GenerateConnectDeviceToken/GenerateConnectDeviceTokenCommand.cs
@@ -0,0 +1,9 @@
+using BarkFluff.Proto.FastAuth;
+using MediatR;
+
+namespace BarkFluff.FastAuth.Features.GenerateConnectDeviceToken;
+
+public class GenerateConnectDeviceTokenCommand : IRequest<GenerateConnectDeviceTokenResponse>
+{
+    
+}
---
+++ Backend/BarkFluff.FastAuth/Program.cs
@@ -0,0 +1,47 @@
+using BarkFluff.FastAuth.Host;
+using BarkFluff.GrpcServer;
+using BarkFluff.GrpcServer.XAuth;
+using BarkFluff.Shared.Identity;
+
+namespace BarkFluff.FastAuth;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.LoadConfiguration(ServiceId.Users);
+        builder.SetRunningAddress(builder.Configuration);
+
+        // Регистрируем gRPC сервисы с интерцепторами
+        builder.Services.AddGrpc(options =>
+        {
+            options.Interceptors.Add<ServerExceptionInterceptor>();
+        });
+        
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+        
+        builder.Services.AddGrpcReflection();
+
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+
+        // Регистрируем аутентификацию и авторизацию
+        builder.Services.AddXAuth(builder.Configuration);
+        
+        
+        var app = builder.Build();
+        
+        app.MapGrpcReflectionService();
+        
+        // Настраиваем middleware pipeline
+        app.UseRouting();
+        
+        app.UseXAuth();
+        
+        // Регистрируем gRPC сервисы
+        app.MapGrpcService<FastAuthApiService>();
+
+        app.Run();
+    }
+}
