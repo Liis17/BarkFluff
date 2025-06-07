cd53aea1300893e597fbf813a0f06b07874b58eb
Li_is <daskent96@gmail.com>
2025-06-07 17:57:51 +0300

    ✨ обновлена версия приложения до 0.0.0.390
    Добавлен класс `Base64ToBitmapSource` для преобразования строк в формате Base64 в `BitmapSource` с обработкой ошибок. Также добавлен класс `VideoCompressor` с асинхронным методом `CompressAsync` для сжатия видео с использованием FFmpeg.

---
+++ BarkFluff.Client.WPF/Services/App/Converter/Base64ToBitmapSource.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace BarkFluff.Client.WPF.Services.App.Converter
+{
+    public class Base64ToBitmapSource
+    {
+        public static BitmapSource? ConvertBase64ToBitmapSource(string base64String)
+        {
+            if (string.IsNullOrEmpty(base64String))
+            {
+                return null;
+            }
+
+            try
+            {
+                byte[] imageBytes = Convert.FromBase64String(base64String);
+
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.StreamSource = ms;
+                    bitmapImage.EndInit();
+                    bitmapImage.Freeze();
+
+                    return bitmapImage;
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show($"Ошибка формата base64: {ex.Message}");
+                return null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при преобразовании base64: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
