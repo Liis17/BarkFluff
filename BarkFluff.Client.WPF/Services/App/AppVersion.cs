402ad6cfef1e1794ce52d7b7041322f9f17eef05
Li_is <daskent96@gmail.com>
2025-06-07 20:03:37 +0300

    ✨ добавлено создание ярлыка приложения в меню "Пуск"
    Добавлен код для создания ярлыка приложения в меню "Пуск" в `App.xaml.cs`, обновлены зависимости в проекте, улучшена аутентификация с помощью новых методов для работы с OTP, а также добавлены конфигурации для поддержки платформы x64.

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-       public static string Version { get; set; } = "0.0.0.390";
+       public static string Version { get; set; } = "0.0.0.392";
         public static string VersionName { get; } = "α";
         public static string AppName
         {
