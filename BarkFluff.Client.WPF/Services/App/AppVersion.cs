f9626a42c9397ce8353ad508211fb83601fabdb7
Li_is <daskent96@gmail.com>
2025-06-13 02:40:02 +0300

    🚀 обновлена версия приложения до 0.0.0.431
    Добавлен дублирующий элемент изображения в TwoFA.xaml и изменено значение версии в AppVersion.cs.

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-       public static string Version { get; set; } = "0.0.0.424";
+       public static string Version { get; set; } = "0.0.0.431";
         public static string VersionName { get; } = "α";
         public static string AppName
         {
