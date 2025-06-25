2f1856641d2442d3a7823cc462a7fc7e2e701fc5
Li_is <daskent96@gmail.com>
2025-06-26 00:11:53 +0300

    ✨ улучшение восстановления пароля и двухфакторной аутентификации
    - 🚀 Улучшено начальное фокусирование в `PasswordReset.xaml.cs` с `VerifyBox0` на `EmailTextBox`.
    - 📝 Добавлено сообщение о недоступности функции повторной отправки кода в методе `ResendCode_Click`.
    - 💻 Добавлен новый стиль `VerificationTextBox` и изменен интерфейс двухфакторной аутентификации в `Login.xaml`.
    - 🛠️ Реализована поддержка двухфакторной аутентификации в `Login.xaml.cs`, добавлены поля и методы для проверки кода 2FA.
    - 🔧 Обновлена версия приложения до 0.0.0.494 в `AppVersion.cs`.

--- BarkFluff.Client.WPF/Pages/SetupPages/Login.xaml.cs
+++ BarkFluff.Client.WPF/Pages/SetupPages/Login.xaml.cs
@@ -15,9 +15,21 @@ namespace BarkFluff.Client.WPF.Pages.SetupPages
         private string _email = string.Empty;
         private string _password = string.Empty;
         private string _otpCode = string.Empty;
+
+        private bool _step2FA = false;
+
+        private TextBox[]? codeBoxes;
         public Login()
         {
             InitializeComponent();
+            Loaded += Login_Loaded;
+        }
+
+        private void Login_Loaded(object sender, RoutedEventArgs e)
+        {
+            OtpBlock.Visibility = Visibility.Collapsed;
+            codeBoxes = new[] { VerifyBox0, VerifyBox1, VerifyBox2, VerifyBox3, VerifyBox4, VerifyBox5 };
+            VerifyBox0.Focus();
         }
 
         private void CreateAccountPageOpen(object sender, RoutedEventArgs e)
@@ -47,9 +59,10 @@ namespace BarkFluff.Client.WPF.Pages.SetupPages
                     _username = UsernameTextBox.Text;
                 }
                 _password = PasswordBox.Password;
-                var response = await App.ServerCommunication.Authorisation(_email, _username, _password, "", App.GParam);
+                var response = await App.ServerCommunication.Authorisation(_email, _username, _password, _otpCode, App.GParam);
                 if (response.getMeOtpCode)
                 {
+                    _step2FA = true;
                     LoginPasswordFields.Visibility = Visibility.Collapsed;
                     OtpBlock.Visibility = Visibility.Visible;
                 }
@@ -93,6 +106,70 @@ namespace BarkFluff.Client.WPF.Pages.SetupPages
         private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show("Раздел помощи в разработке, не скучайте )", "Информация", MessageBoxButton.AbortRetryIgnore, MessageBoxImage.Information);
+        }
+
+
+        private void VerifyBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox current = sender as TextBox;
+            if (current.Text.Length == 1)
+                current.Select(1, 0); // Чтобы курсор не прыгал
+            else
+                current.SelectAll();
+        }
+        private async void VerifyBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox current = sender as TextBox;
+            if (current.Text.Length == 1)
+            {
+                int index = Array.IndexOf(codeBoxes, current);
+                if (index < codeBoxes.Length - 1)
+                    codeBoxes[index + 1].Focus();
+                else
+                    current.Select(1, 0); // Не прыгать в конец
+            }
+
+            if (codeBoxes.All(b => b.Text.Length == 1))
+            {
+                string code = string.Concat(codeBoxes.Select(b => b.Text));
+                try
+                {
+
+                    await App.ServerCommunication.OtpAccept(App.GParam, code); // Отправка кода на сервер
+
+                }
+                catch
+                {
+
+                    return;
+                }
+
+            }
+        }
+        private void VerifyBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox current = sender as TextBox;
+
+            if (e.Key == Key.Back)
+            {
+                if (current.Text.Length == 0)
+                {
+                    int index = Array.IndexOf(codeBoxes, current);
+                    if (index > 0)
+                    {
+                        TextBox prev = codeBoxes[index - 1];
+                        prev.Focus();
+                        prev.SelectAll();
+                    }
+                }
+            }
+
+            if (e.Key == Key.Tab)
+                e.Handled = true;
+        }
+        private void VerifyBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"^\d$");
         }
     }
 }
