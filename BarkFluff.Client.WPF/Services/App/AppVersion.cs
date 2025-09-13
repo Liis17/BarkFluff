a71847476bede3bef973385239196ea232fdb0db
Li_is <daskent96@gmail.com>
2025-09-13 16:47:16 +0300

    ✨ добавить проверку пустого текста и типа debug в dropmessage
    ♻️ обновлена версия приложения в AppVersion.cs
    🐛 исправлено отображение версии в UI после изменения версии
    ✨ добавлена проверка пустого текста и типа Debug в DropMessage.cs, такие сообщения игнорируются

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.995";
+        public static string Version { get; set; } = "0.0.0.1027";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
