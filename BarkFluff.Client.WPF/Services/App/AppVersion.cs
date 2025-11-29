b5292518717e1e505c7bde4b1ecea517be2b0f1c
Li_is <daskent96@gmail.com>
2025-11-29 15:00:00 +0300

    ✨ Обновлен интерфейс и логика приложения
    - [MessengerPage] Добавлен элемент для отображения аватара пользователя с локальным ресурсом.
    - [MessengerPage] Изменен обработчик событий для блока обновлений.
    - [MessengerPage] Заменена кнопка аватара на новый элемент с улучшенной логикой.
    - [MessengerPage] Добавлена обработка тегов в методе `OpenCenterBlock`.
    - [AppVersion] Обновлен номер версии приложения.
    - [Profile] Обновлены текстовые блоки для отображения имени, имени пользователя и статуса.

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1081";
+        public static string Version { get; set; } = "0.0.0.1084";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
