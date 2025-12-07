166e96838e2940941d1e5764321c47e0d0b26470
Li_is <daskent96@gmail.com>
2025-12-07 05:25:21 +0300

    ✨ Улучшен интерфейс и обработка сообщений
    - [Frontend] Обновлены импорты в `MessengerPage.xaml.cs` для унификации типов данных.
    - [Frontend] Добавлена логика разделителей дат в интерфейсе сообщений.
    - [Frontend] Настроена обработка входящих и исходящих сообщений, включая отображение состояния отправки.
    - [Frontend] Добавлена обработка ошибок при отправке сообщений.
    - [Frontend] Реализовано сохранение сообщений в кэш и обновление списка чатов после отправки.
    - [Frontend] Добавлена проверка на дублирование разделителей дат.
    - [Core] Обновлена версия приложения до `0.0.0.1179`.
    - [Backend] Добавлена зависимость `System.Drawing.Common` в проекте.
    - [Backend] Добавлен импорт коллекций в `ImageProcessor.cs`.

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1176";
+        public static string Version { get; set; } = "0.0.0.1179";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
