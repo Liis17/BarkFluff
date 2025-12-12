d84ba5810b032a2069f54bd542563034b9c4fbf7
Li_is <daskent96@gmail.com>
2025-12-12 18:45:35 +0300

    🟢 Оптимизирована обработка read receipts в клиенте
    - [Client.WPF] Добавлен буфер для оптимизации обновления статуса прочтения сообщений.
    - [Client.WPF] Удалена дублирующая логика подписки на уведомления о прочтении для отдельных чатов.
    - [Client.WPF] Оставлена только глобальная подписка на уведомления о прочтении.
    - [Client.WPF] Обновлена обработка событий для снижения нагрузки на интерфейс.
    - [Client.WPF] Исправлен импорт зависимостей в ReadByConsumer.
    - [Client.WPF] Обновлен номер версии приложения.

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1215";
+        public static string Version { get; set; } = "0.0.0.1222";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
