2213498d43b09fe21699dffc7ddfb5b05bb67ef4
Li_is <daskent96@gmail.com>
2025-12-17 02:14:28 +0300

    🛠️ Обновлена логика ярлыков и версии приложения
    - [Client.WPF] Настроено раздельное создание ярлыков для разных сборок через константу в App.xaml.cs
    - [Client.WPF] Обновлен номер версии приложения в AppVersion.cs

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1303";
+        public static string Version { get; set; } = "0.0.0.1304";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
