3dac183addd666e89eea8f9819c508e642adfc08
Li_is <daskent96@gmail.com>
2025-12-21 16:03:05 +0300

    🆕 Добавлен проект обновления и обновлены стили
    - [Root] В решение добавлен новый проект консольного обновления
    - [Root] В BarkFluff.sln настроены секции для поддержки нового проекта
    - [Client] Обновлен номер версии приложения в AppVersion.cs
    - [Client] Исправлено форматирование и обновлены параметры стилей в Styles.xaml

---
+++ Barkfluff.Updater.CLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Barkfluff.Updater.CLI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Barkfluff Updater CLI");
+            Console.ReadLine();
+        }
+    }
+}
