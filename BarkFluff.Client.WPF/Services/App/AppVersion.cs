3438f39a22a8d44b9cc7260f455cbae7f8d7e764
Li_is <daskent96@gmail.com>
2025-12-24 21:41:02 +0300

    🖼️ Обновлены стили и обработчики интерфейса
    - [Client.WPF] Обновлён внешний вид элементов в MessengerPage.xaml и ChatItem.xaml
    - [Client.WPF] Изменён обработчик события для кнопки настроек в SideBar.xaml и добавлен соответствующий метод в SideBar.xaml.cs
    - [Client.WPF] Обновлён номер версии приложения в AppVersion.cs
    - [Client.WPF] Улучшена визуализация аватара и панели сообщений
    - [Client.WPF] Удалён неиспользуемый импорт в SideBar.xaml.cs

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1336";
+        public static string Version { get; set; } = "0.0.0.1345";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
