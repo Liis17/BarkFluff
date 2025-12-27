5a901a20356914a8ddcbba73a7b3fef568acb635
Li_is <daskent96@gmail.com>
2025-12-28 00:53:10 +0300

    триггер

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1370";
+        public static string Version { get; set; } = "0.0.0.1371";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
