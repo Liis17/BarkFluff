ffe8e02e01e07b54478137aea983db588f2d62d4
Li_is <daskent96@gmail.com>
2025-12-28 01:04:17 +0300

    sad

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1371";
+        public static string Version { get; set; } = "0.0.0.1374";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
