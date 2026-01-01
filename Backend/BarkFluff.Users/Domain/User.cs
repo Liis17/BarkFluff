32766271f08150c249fb50e5822918ba5bd7998b
Li_is <daskent96@gmail.com>
2026-01-01 22:32:49 +0300

    🗄️ Добавлено ограничение хранилища и улучшен прогрессбар
    - [Backend] Добавлено миграционное обновление для ограничения хранилища пользователей
    - [Backend] Обновлена доменная модель пользователя для поддержки лимита хранилища
    - [Client.WPF] Переработан визуал и логика панели прогресса использования места
    - [Client.WPF] Улучшена анимация прогресс-бара и управление циклами
    - [API] Выполнен рефакторинг сервисного слоя и обновлены зависимости
    - [Client.WPF] Обновлен номер версии приложения

--- Backend/BarkFluff.Users/Domain/User.cs
+++ Backend/BarkFluff.Users/Domain/User.cs
@@ -6,22 +6,22 @@ public class User
 {
     [Key]
     public long Id { get; set; }
-    
+
     public string Username { get; set; }
-    
+
     public string FirstName { get; set; }
-    
+
     public string LastName { get; set; }
-    
+
     public DateTime RegistrationDate { get; set; }
-    
+
     public UserContact Contact { get; set; }
-    
+
     public bool IsDraft { get; set; }
-    
+
     public string? ProfilePicture { get; set; }
-    
+
     public string? Bio { get; set; }
-    
+
     public string? ProfilePicturePreviewUrl { get; set; }
 }
---
+++ Backend/BarkFluff.Users/Migrations/20260101192152_AddStorageLimitGb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarkFluff.Users.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStorageLimitGb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StorageLimitGb",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StorageLimitGb",
+                table: "Users");
+        }
+    }
+}
