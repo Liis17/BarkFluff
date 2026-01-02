e3267231f291952831c7b1bb50d92b96bd3b375d
Li_is <daskent96@gmail.com>
2026-01-02 13:02:51 +0300

    🖌️ Улучшено оформление и взаимодействие настроек
    - [Client.WPF] Добавлен новый стиль для заголовков в настройках
    - [Client.WPF] Обновлён номер версии приложения
    - [Client.WPF] Применён новый стиль к заголовкам разделов в настройках
    - [Client.WPF] Упрощена структура разметки элементов меню настроек
    - [Client.WPF] Реализован анимированный эффект размытия иконок при наведении
    - [Client.WPF] Изменён порядок подписки на события в контроле меню настроек
    - [Client.WPF] Добавлены свойства для улучшения внешнего вида и взаимодействия контролов меню настроек

--- BarkFluff.Client.WPF/UserControls/SettingsPages/SettingsMenuBlock.xaml.cs
+++ BarkFluff.Client.WPF/UserControls/SettingsPages/SettingsMenuBlock.xaml.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BarkFluff.Client.WPF.UserControls.SettingsPages
 {
@@ -66,9 +58,9 @@ namespace BarkFluff.Client.WPF.UserControls.SettingsPages
         public SettingsMenuBlock()
         {
             InitializeComponent();
+            this.MouseLeftButtonDown += OnMouseLeftButtonDown;
             this.MouseEnter += OnMouseEnter;
             this.MouseLeave += OnMouseLeave;
-            this.MouseLeftButtonDown += OnMouseLeftButtonDown;
         }
 
         private void OnMouseEnter(object sender, MouseEventArgs e)
