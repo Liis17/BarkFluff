e1c603afd03922239d1e4cd39758173798468426
Li_is <daskent96@gmail.com>
2026-01-04 06:25:26 +0300

    🖼️ Обновлён интерфейс и иконка сайта
    - [WebServer] Выполнен редизайн страниц и обновлён стиль интерфейса
    - [WebServer] Добавлен и обновлён favicon.ico, настроена его выдача через контроллер
    - [WebServer] Изменён способ загрузки обновлений, обновлены ссылки
    - [WebServer] Улучшена адаптивность и взаимодействие с пользователем
    - [WebServer] Проведён рефакторинг стилей и логики отображения элементов
    - [WebServer] Обновлены правила включения файлов в проект

---
+++ Barkfluff.WebServer/Controllers/FaviconController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Barkfluff.WebServer.Controllers
+{
+    [ApiController]
+    public class FaviconController : ControllerBase
+    {
+        [HttpGet("favicon.ico")]
+        public IActionResult GetFavicon()
+        {
+            var assemblyLocation = AppContext.BaseDirectory;
+            var faviconPath = Path.Combine(assemblyLocation, "files", "favicon.ico");
+
+            if (!System.IO.File.Exists(faviconPath))
+            {
+                return NotFound("Favicon not found");
+            }
+
+            var fileBytes = System.IO.File.ReadAllBytes(faviconPath);
+            return File(fileBytes, "application/octet-stream", "favicon.ico");
+        }
+    }
+}
