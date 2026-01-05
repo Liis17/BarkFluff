847bc764898596ac219d5fca5395bcac99155c98
Li_is <daskent96@gmail.com>
2026-01-05 15:44:46 +0300

    🛠️ Обновлены версия и элементы интерфейса
    - [Client] Обновлён идентификатор версии в AppVersion.cs
    - [Client] Добавлен элемент отображения использования пространства в Settings.xaml
    - [Client] Изменено форматирование структуры разметки в Settings.xaml

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1448";
+        public static string Version { get; set; } = "0.0.0.1450";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
