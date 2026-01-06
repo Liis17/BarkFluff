e6d9b18e2007720c295a6f07df864ebb2bedb910
Li_is <daskent96@gmail.com>
2026-01-06 23:18:04 +0300

    📝 Обновлена версия приложения
    - [Client] В AppVersion.cs обновлено значение версии приложения.

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1508";
+        public static string Version { get; set; } = "0.0.0.1509";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
