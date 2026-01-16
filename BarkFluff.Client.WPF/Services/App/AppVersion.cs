7892e0e3e4079ad62e0465b01543a9d265c859ba
Li_is <daskent96@gmail.com>
2026-01-17 00:02:51 +0300

    🖼️ Улучшён интерфейс предпросмотра вложений
    - [Client] Обновлён идентификатор версии в AppVersion.cs.
    - [Client] Переработан внешний вид окна предпросмотра вложений в AttachmentPreviewOverlay.xaml.
    - [Client] Настроено отображение и состояние кнопки добавления вложений.
    - [Client] Упрощена логика отправки вложений, удалён выпадающий список опций.
    - [Client] Добавлены новые элементы управления для выбора способа отправки.
    - [Client] Улучшено взаимодействие пользователя с окном предпросмотра вложений.

--- BarkFluff.Client.WPF/Services/App/AppVersion.cs
+++ BarkFluff.Client.WPF/Services/App/AppVersion.cs
@@ -2,7 +2,7 @@ namespace BarkFluff.Client.WPF
 {
     public class AppVersion
     {
-        public static string Version { get; set; } = "0.0.0.1563";
+        public static string Version { get; set; } = "0.0.0.1564";
 #if DEBUG
         public static string VersionType { get; set; } = "Dev";
 #else
