eedaab1657de87e37db8b77825a4a02b9e2717fa
Li_is <daskent96@gmail.com>
2026-02-01 11:42:06 +0300

    🧹 Удалены отладочные сообщения и упрощена инициализация
    - [DBEditor] Удалены всплывающие отладочные сообщения из основных окон и моделей представления.
    - [DBEditor] Упрощена логика инициализации окон за счёт удаления временных блоков обработки ошибок.
    - [DBEditor] Исключения более не перехватываются локально в конструкторах, что повысило прозрачность выполнения.
    - [DBEditor] Повышена чистота и читаемость кода за счёт удаления временного отладочного функционала.

--- BarkFluff.DBEditor/Views/MainWindow.xaml.cs
+++ BarkFluff.DBEditor/Views/MainWindow.xaml.cs
@@ -11,24 +11,12 @@ namespace BarkFluff.DBEditor.Views
     {
         public MainWindow(SavedAccount account, CredentialsService credentialsService)
         {
-            try
-            {
-                MessageBox.Show("MainWindow constructor started", "Debug", MessageBoxButton.OK, MessageBoxImage.Information);
-                InitializeComponent();
-                MessageBox.Show("InitializeComponent done", "Debug", MessageBoxButton.OK, MessageBoxImage.Information);
-                ApplicationThemeManager.Apply(this);
-                MessageBox.Show("ApplicationThemeManager done", "Debug", MessageBoxButton.OK, MessageBoxImage.Information);
-                DataContext = new MainViewModel(account, credentialsService, this);
-                MessageBox.Show("MainViewModel created", "Debug", MessageBoxButton.OK, MessageBoxImage.Information);
+            InitializeComponent();
+            ApplicationThemeManager.Apply(this);
+            DataContext = new MainViewModel(account, credentialsService, this);
 
-                // Shutdown app when this window closes
-                Closed += (s, e) => Application.Current.Shutdown();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error in MainWindow constructor: {ex.Message}\n\n{ex.StackTrace}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                throw;
-            }
+            // Shutdown app when this window closes
+            Closed += (s, e) => Application.Current.Shutdown();
         }
     }
 }
