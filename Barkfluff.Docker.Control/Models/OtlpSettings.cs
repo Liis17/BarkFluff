91c13d49b1749e8677195537cf29e956f75677e5
Li_is <daskent96@gmail.com>
2026-02-13 00:29:09 +0300

    🗑️ Удалены устаревшие компоненты и страницы
    - [Backend] Удалены файлы обработки OTLP-метрик и настройки (MetricsEndpoints.cs, OtlpSettings.cs, metrics_service.proto, metrics.proto, common.proto, resource.proto)
    - [Frontend] Удалены административные HTML-страницы панели управления (db.html, docker.html, statistic.html, users.html)

--- Barkfluff.Docker.Control/Models/OtlpSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Barkfluff.Docker.Control.Models;
-
-public class OtlpSettings
-{
-    public const string SectionName = "Otlp";
-    public string? ApiKey { get; set; }
-    public bool Enabled { get; set; } = true;
-}
