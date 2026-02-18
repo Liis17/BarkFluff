7dde5f8b0f8080e30b56d48cf2d450d89e2c4552
Li_is <daskent96@gmail.com>
2026-02-18 17:06:39 +0300

    добавлена адаптивная тема (почти)

--- Barkfluff.Client.Android/app/src/main/java/com/barkfluff/client/MainActivity.kt
+++ Barkfluff.Client.Android/app/src/main/java/com/barkfluff/client/MainActivity.kt
@@ -2,10 +2,14 @@ package com.barkfluff.client
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.color.DynamicColors
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Apply dynamic colors if available (Android 12+)
+        DynamicColors.applyToActivityIfAvailable(this)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
