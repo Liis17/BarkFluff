a18a25069848263c4736fb4c76f1e6bab78dcf4e
Li_is <daskent96@gmail.com>
2026-02-18 17:42:37 +0300

    реализованно подтягивание системной темы

--- Barkfluff.Client.Android/app/src/main/java/com/barkfluff/client/MainActivity.kt
+++ Barkfluff.Client.Android/app/src/main/java/com/barkfluff/client/MainActivity.kt
@@ -7,9 +7,7 @@ import com.google.android.material.color.DynamicColors
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Apply dynamic colors if available (Android 12+)
         DynamicColors.applyToActivityIfAvailable(this)
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
