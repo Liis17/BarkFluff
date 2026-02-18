2318f54cb8ed33d9b2f95ba7d14d057b172b654c
Li_is <daskent96@gmail.com>
2026-02-18 16:26:08 +0300

    ура, первый билд который запустился

--- Barkfluff.Client.Android/app/build.gradle.kts
+++ Barkfluff.Client.Android/app/build.gradle.kts
@@ -43,9 +43,5 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.cardview:cardview:1.0.0")
 }
