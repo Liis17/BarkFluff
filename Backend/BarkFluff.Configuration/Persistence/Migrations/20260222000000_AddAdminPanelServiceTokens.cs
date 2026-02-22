7daffd2919a3420e7fc1ab99a1343ed1e18b4817
Li_is <daskent96@gmail.com>
2026-02-22 04:30:27 +0300

    фиксы работы админ панели

---
+++ Backend/BarkFluff.Configuration/Persistence/Migrations/20260222000000_AddAdminPanelServiceTokens.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarkFluff.Configuration.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminPanelServiceTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                INSERT INTO ""Configurations"" (""Section"", ""Key"", ""Value"", ""EditedAt"", ""EditedBy"", ""EditedFrom"", ""ServiceId"")
+                VALUES
+                    -- FilesService для AdminPanel (ServiceId = 0)
+                    ('FilesService', 'Token', '', NOW(), 'system', 'migration', 0),
+                    ('FilesService', 'Host', 'http://files:7005', NOW(), 'system', 'migration', 0);
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM ""Configurations""
+                WHERE ""Section"" = 'FilesService' AND ""ServiceId"" = 0 AND ""EditedBy"" = 'system' AND ""EditedFrom"" = 'migration';
+            ");
+        }
+    }
+}
