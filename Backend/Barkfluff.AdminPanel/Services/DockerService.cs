ea26f01feee692045864729b937f441914386e9d
Li_is <daskent96@gmail.com>
2026-02-22 06:03:48 +0300

    прокинут env файл

--- Backend/Barkfluff.AdminPanel/Services/DockerService.cs
+++ Backend/Barkfluff.AdminPanel/Services/DockerService.cs
@@ -155,11 +155,11 @@ public class DockerService
             _logger.LogInformation("Обновление контейнера {ContainerName} (сервис: {ServiceName})", containerName, serviceName);
 
             // 1. Pull нового образа через docker compose pull <service>
-            await RunDockerComposeCommandAsync("-f", "/docker-compose.yml", "pull", serviceName);
+            await RunDockerComposeCommandAsync("--env-file", "/.env", "-f", "/docker-compose.yml", "pull", serviceName);
             _logger.LogInformation("Образ для сервиса {ServiceName} успешно обновлен", serviceName);
 
             // 2. Пересоздать контейнер через docker compose up --force-recreate --build -d <service>
-            await RunDockerComposeCommandAsync("-f", "/docker-compose.yml", "up", "--force-recreate", "--build", "-d", serviceName);
+            await RunDockerComposeCommandAsync("--env-file", "/.env", "-f", "/docker-compose.yml", "up", "--force-recreate", "--build", "-d", serviceName);
             _logger.LogInformation("Контейнер {ContainerName} успешно пересоздан", containerName);
 
             // 3. Очистить неиспользуемые образы через docker image prune -f
