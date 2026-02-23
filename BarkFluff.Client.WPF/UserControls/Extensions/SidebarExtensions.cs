7e85585844db9637713255865866ade4fe3f9a88
Li_is <daskent96@gmail.com>
2026-02-24 00:20:56 +0300

    добавлены новые стили добавлен расширитель возмоджностей стилей изменен стиль кнопок в сайдбаре сайдбар теперь поддерживает светлую тему

---
+++ BarkFluff.Client.WPF/UserControls/Extensions/SidebarExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+using Wpf.Ui.Controls;
+
+namespace BarkFluff.Client.WPF.UserControls.Extensions
+{
+    public class SidebarExtensions
+    {
+        public static readonly DependencyProperty IconSymbolProperty =
+            DependencyProperty.RegisterAttached(
+                "IconSymbol",
+                typeof(SymbolRegular),
+                typeof(SidebarExtensions),
+                new PropertyMetadata(SymbolRegular.Bookmark20));
+
+        public static SymbolRegular GetIconSymbol(DependencyObject obj)
+            => (SymbolRegular)obj.GetValue(IconSymbolProperty);
+
+        public static void SetIconSymbol(DependencyObject obj, SymbolRegular value)
+            => obj.SetValue(IconSymbolProperty, value);
+    }
+}
