40e2d583fc72b53621b67d84248cf8373bb15b1c
Li_is <daskent96@gmail.com>
2026-03-08 16:12:36 +0300

    фикс фикса фикса

--- Windows/BarkFluff.Client.WPF/App.xaml.cs
+++ Windows/BarkFluff.Client.WPF/App.xaml.cs
@@ -388,7 +388,7 @@ namespace BarkFluff.Client.WPF
             versionParts[3] = buildNumber.ToString();
             AppVersion.Version = string.Join(".", versionParts);
 
-            var versionFile = "K:\\source\\BarkFluff\\BarkFluff.Client.WPF\\Services\\App\\AppVersion.cs";
+            var versionFile = "K:\\source\\BarkFluff\\Windows\\BarkFluff.Client.WPF\\Services\\App\\AppVersion.cs";
             var lines = System.IO.File.ReadAllLines(versionFile);
             for (int i = 0; i < lines.Length; i++)
             {
