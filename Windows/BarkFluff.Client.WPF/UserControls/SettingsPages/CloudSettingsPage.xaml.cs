cc16e7f0710428908dafa6949dcbf5c6c74f0854
Li_is <daskent96@gmail.com>
2026-03-15 16:19:05 +0300

    исправлено отображение разных элементов в настройках

--- Windows/BarkFluff.Client.WPF/UserControls/SettingsPages/CloudSettingsPage.xaml.cs
+++ Windows/BarkFluff.Client.WPF/UserControls/SettingsPages/CloudSettingsPage.xaml.cs
@@ -2,6 +2,7 @@ using BarkFluff.Proto.Files;
 
 using System.Globalization;
 using System.Windows;
+using System.Windows.Media;
 
 namespace BarkFluff.Client.WPF.UserControls.SettingsPages
 {
@@ -35,21 +36,8 @@ namespace BarkFluff.Client.WPF.UserControls.SettingsPages
                 double percent = totalBytes > 0 ? (double)usedBytes / totalBytes * 100 : 0;
                 UsagePercentText.Text = $"{percent:F1}% использовано";
 
-                // Обновить прогресс-бар
-                Dispatcher.Invoke(() =>
-                {
-                    var parent = UsageBar.Parent as FrameworkElement;
-                    if (parent != null)
-                    {
-                        double barWidth = parent.ActualWidth > 0
-                            ? parent.ActualWidth * (percent / 100)
-                            : 0;
-                        UsageBar.Width = Math.Max(0, barWidth);
-                    }
-                });
-
                 // Разбивка по типам
-                long imageSize = 0, videoSize = 0, documentSize = 0;
+                long imageSize = 0, videoSize = 0, documentSize = 0, audioSize = 0, voiceSize = 0;
                 foreach (var st in userSize.storageByType)
                 {
                     switch (st.Key)
@@ -66,12 +54,34 @@ namespace BarkFluff.Client.WPF.UserControls.SettingsPages
                         case UploadFileType.MessageAttachmentDocument:
                             documentSize += st.Value;
                             break;
+                        case UploadFileType.MessageAttachmentAudio:
+                            audioSize += st.Value;
+                            break;
+                        case UploadFileType.MessageAttachmentVoice:
+                            voiceSize += st.Value;
+                            break;
                     }
                 }
 
                 if (imageSize > 0) { ImagesSize.Text = FormatBytes(imageSize); ImagesBorder.Visibility = Visibility.Visible; }
                 if (videoSize > 0) { VideosSize.Text = FormatBytes(videoSize); VideosBorder.Visibility = Visibility.Visible; }
                 if (documentSize > 0) { DocumentsSize.Text = FormatBytes(documentSize); DocumentsBorder.Visibility = Visibility.Visible; }
+                if (audioSize > 0) { AudioSize.Text = FormatBytes(audioSize); AudioBorder.Visibility = Visibility.Visible; }
+                if (voiceSize > 0) { VoiceSize.Text = FormatBytes(voiceSize); VoiceBorder.Visibility = Visibility.Visible; }
+
+                // Обновить сегментный прогресс-бар
+                Dispatcher.Invoke(() =>
+                {
+                    StorageProgress.ClearSegments();
+                    if (imageSize > 0) StorageProgress.AddSegment(Math.Max(1, (int)(imageSize / 1024)), new SolidColorBrush(Color.FromRgb(0x57, 0xBB, 0x62)));
+                    if (videoSize > 0) StorageProgress.AddSegment(Math.Max(1, (int)(videoSize / 1024)), new SolidColorBrush(Color.FromRgb(0x3D, 0x50, 0xB7)));
+                    if (documentSize > 0) StorageProgress.AddSegment(Math.Max(1, (int)(documentSize / 1024)), new SolidColorBrush(Color.FromRgb(0xCA, 0x6D, 0x34)));
+                    if (audioSize > 0) StorageProgress.AddSegment(Math.Max(1, (int)(audioSize / 1024)), new SolidColorBrush(Color.FromRgb(0x9B, 0x59, 0xB6)));
+                    if (voiceSize > 0) StorageProgress.AddSegment(Math.Max(1, (int)(voiceSize / 1024)), new SolidColorBrush(Color.FromRgb(0x1A, 0xBC, 0x9C)));
+                    long freeSpace = totalBytes - usedBytes;
+                    if (freeSpace > 0) StorageProgress.AddSegment(Math.Max(1, (int)(freeSpace / 1024)), StorageProgress.EmptyBrush);
+                    StorageProgress.AnimStart();
+                });
             }
             catch
             {
