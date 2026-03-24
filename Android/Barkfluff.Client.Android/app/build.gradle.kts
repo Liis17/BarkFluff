03dba19ec3cbe0656e6bcd9a70a2b1136040c300
Li_is <daskent96@gmail.com>
2026-03-25 00:33:05 +0300

    добавлены новые иконки устройств

--- Android/Barkfluff.Client.Android/app/build.gradle.kts
+++ Android/Barkfluff.Client.Android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.3"
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
