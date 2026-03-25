f9970b8b834dd332db333ec431321e08368256f1
Li_is <daskent96@gmail.com>
2026-03-26 00:20:01 +0300

    показывает бейдж того что доступно обновление

--- Android/Barkfluff.Client.Android/app/build.gradle.kts
+++ Android/Barkfluff.Client.Android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.4"
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
