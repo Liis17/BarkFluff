43e292ea36c38222e346fcf094e9947dac8e6952
Li_is <daskent96@gmail.com>
2026-03-31 18:29:07 +0300

    поднята версия

--- Android/Barkfluff.Client.Android/app/build.gradle.kts
+++ Android/Barkfluff.Client.Android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.10"
+        versionName = "0.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
