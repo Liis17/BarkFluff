a99704ea4082f0e995879a3fb063d157603da026
Li_is <daskent96@gmail.com>
2026-03-31 19:16:21 +0300

    1

--- Android/Barkfluff.Client.Android/app/build.gradle.kts
+++ Android/Barkfluff.Client.Android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.11"
+        versionName = "0.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
