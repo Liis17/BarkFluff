82968f09682f6be3af6222c1d7d13184382ed61e
Li_is <daskent96@gmail.com>
2026-04-12 23:48:34 +0300

    изменена базовая версия в файлах сборки градла

--- Android/Barkfluff.Client.Android/app/build.gradle.kts
+++ Android/Barkfluff.Client.Android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.13"
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
