cd6cdc856d42dbef06b5d543a4f3eba94fd6edd2
Li_is <daskent96@gmail.com>
2026-04-19 15:10:47 +0300

    Отображение версий и i18n на главной странице
    Добавлен серверный API и клиентская логика для отображения актуальных версий Windows/Android (релиз/бета) на главной. Реализована система i18n (русский/английский) с динамическим переключением языка и автосохранением. Автоматически определяется платформа пользователя, скрывается CLI для Android/iOS. Проведен рефакторинг структуры и улучшена доступность.

---
+++ Backend/Barkfluff.WebServer/Controllers/VersionApiController.cs
@@ -0,0 +1,23 @@
+using Barkfluff.WebServer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barkfluff.WebServer.Controllers;
+
+[ApiController]
+public class VersionApiController : ControllerBase
+{
+    private readonly VersionStore _store;
+
+    public VersionApiController(VersionStore store) => _store = store;
+
+    [HttpGet("/api/versions")]
+    public IActionResult GetVersions()
+    {
+        var (androidRelease, androidBeta, windowsRelease, windowsBeta) = _store.GetAll();
+        return Ok(new
+        {
+            android = new { release = androidRelease, beta = androidBeta },
+            windows = new { release = windowsRelease, beta = windowsBeta }
+        });
+    }
+}
