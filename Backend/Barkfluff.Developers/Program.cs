ca78ebe0cea1079dcb739db0724e2bda1b582930
Li_is <daskent96@gmail.com>
2026-04-19 04:17:12 +0300

    Добавлен Barkfluff.Developers: портал и сборка
    Внедрён сервис Barkfluff.Developers с порталом для разработчиков (index.html), отдачей proto-файлов и документации. Добавлены Dockerfile, docker-compose и интеграция в CI/CD: ручное одобрение через Telegram, сборка и пуш образа. Удалены шаблонные контроллеры и классы, обновлён .csproj для копирования статических файлов.

--- Backend/Barkfluff.Developers/Program.cs
+++ Backend/Barkfluff.Developers/Program.cs
@@ -1,4 +1,3 @@
-
 namespace Barkfluff.Developers
 {
     public class Program
@@ -7,25 +6,18 @@ namespace Barkfluff.Developers
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var port = int.Parse(Environment.GetEnvironmentVariable("DEVELOPERS_PORT") ?? "8080");
+
+            builder.WebHost.ConfigureKestrel(serverOptions =>
+            {
+                serverOptions.ListenAnyIP(port);
+            });
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
+            app.UseRouting();
             app.MapControllers();
 
             app.Run();
