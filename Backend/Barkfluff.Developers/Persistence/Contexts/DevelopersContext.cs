24728524f9f39f49dd78443426940240f5f4485a
Li_is <daskent96@gmail.com>
2026-04-24 01:48:34 +0300

    Явное маппинг свойств, SSL и gRPC в nginx, порт 4425
    В DbContext добавлено явное сопоставление свойств сущностей с колонками БД и типами jsonb. В docker-compose-dev.yml открыт порт 4425 для сервиса developers. В nginx.conf настроен SSL, изменён домен, обновлены пути и проксирование gRPC с использованием grpc_pass и новых заголовков.

--- Backend/Barkfluff.Developers/Persistence/Contexts/DevelopersContext.cs
+++ Backend/Barkfluff.Developers/Persistence/Contexts/DevelopersContext.cs
@@ -16,20 +16,39 @@ public class DevelopersContext : DbContext
         modelBuilder.Entity<DocumentationSection>(e =>
         {
             e.HasKey(x => x.Id);
+            e.Property(x => x.Id).HasColumnName("id");
+            e.Property(x => x.Key).HasColumnName("key");
+            e.Property(x => x.Title).HasColumnName("title");
+            e.Property(x => x.Type).HasColumnName("type");
+            e.Property(x => x.Order).HasColumnName("order");
+            e.Property(x => x.Content).HasColumnName("content").HasColumnType("jsonb");
+            e.Property(x => x.CreatedAt).HasColumnName("created_at");
+            e.Property(x => x.UpdatedAt).HasColumnName("updated_at");
             e.HasIndex(x => x.Key).IsUnique();
-            e.Property(x => x.Content).HasColumnType("jsonb");
         });
 
         modelBuilder.Entity<ProtoMetadata>(e =>
         {
             e.HasKey(x => x.Id);
+            e.Property(x => x.Id).HasColumnName("id");
+            e.Property(x => x.FileName).HasColumnName("file_name");
+            e.Property(x => x.DisplayName).HasColumnName("display_name");
+            e.Property(x => x.Slug).HasColumnName("slug");
+            e.Property(x => x.Order).HasColumnName("order");
+            e.Property(x => x.RpcDescriptions).HasColumnName("rpc_descriptions").HasColumnType("jsonb");
+            e.Property(x => x.CreatedAt).HasColumnName("created_at");
+            e.Property(x => x.UpdatedAt).HasColumnName("updated_at");
             e.HasIndex(x => x.FileName).IsUnique();
-            e.Property(x => x.RpcDescriptions).HasColumnType("jsonb");
         });
 
         modelBuilder.Entity<ErrorCodeEntry>(e =>
         {
             e.HasKey(x => x.Id);
+            e.Property(x => x.Id).HasColumnName("id");
+            e.Property(x => x.Code).HasColumnName("code");
+            e.Property(x => x.ExceptionName).HasColumnName("exception_name");
+            e.Property(x => x.Description).HasColumnName("description");
+            e.Property(x => x.Domain).HasColumnName("domain");
             e.HasIndex(x => x.Code).IsUnique();
         });
     }
