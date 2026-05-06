2e7401e09acd44f086b05b1fb88e09197c92333a
Li_is <daskent96@gmail.com>
2026-05-06 23:37:21 +0300

    фикс проблем выявленых при аудите beacon

--- Backend/BarkFluff.Beacon/Configurations/ServerColorSettings.cs
+++ Backend/BarkFluff.Beacon/Configurations/ServerColorSettings.cs
@@ -2,9 +2,9 @@ namespace BarkFluff.Beacon.Configurations;
 
 public class ServerColorSettings
 {
-    public string Lite { get; set; }
+    public string Lite { get; set; } = string.Empty;
 
-    public string Main { get; set; }
+    public string Main { get; set; } = string.Empty;
 
-    public string Hard { get; set; }
+    public string Hard { get; set; } = string.Empty;
 }
